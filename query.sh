04ce1156fd5b59fcdfe1228ad03e8b407c225c4a
Mario Scrocca <mario.scrocca@gmail.com>
2020-04-01 20:06:19 +0200

    Update query.sh

--- query.sh
+++ query.sh
@@ -9,9 +9,9 @@ do
     then
         echo "Neo4j Ready - Adding constraints"
         # Create constraint on conversation ids to avoid multiple insertion when starting containers multiple times
-        curl -X POST -H 'Content-type: application/json' http://neo4j:action@coney-neo4j:7474/db/data/transaction/commit -d '{"statements": [{"statement": "CREATE CONSTRAINT ON (c:Conversation) ASSERT c.conv_id IS UNIQUE;"}]}'
+        curl -X POST -H 'Content-type: application/json' http://neo4j:coney@coney-neo4j:7474/db/data/transaction/commit -d '{"statements": [{"statement": "CREATE CONSTRAINT ON (c:Conversation) ASSERT c.conv_id IS UNIQUE;"}]}'
         echo "Neo4j Ready - Add coney-data"
-        curl -X POST -H 'Content-type: application/json' http://neo4j:action@coney-neo4j:7474/db/data/transaction/commit -d '{"statements": [{"statement": "MERGE (c:Conversation {conv_id: \"id--193834919-1563808972845\", conv_title: \"Coney-demo\", conv_type: \"\", json_url: \"/opt/coney-data/Coney-demo.txt\", lang: \"en\", accessLevel: 1, status: \"saved\"});"}]}'
+        curl -X POST -H 'Content-type: application/json' http://neo4j:coney@coney-neo4j:7474/db/data/transaction/commit -d '{"statements": [{"statement": "MERGE (c:Conversation {conv_id: \"id--193834919-1563808972845\", conv_title: \"Coney-demo\", conv_type: \"\", json_url: \"/opt/coney-data/Coney-demo.txt\", lang: \"en\", accessLevel: 1, status: \"saved\"});"}]}'
         break
     else
         echo "Neo4j Not Ready"
