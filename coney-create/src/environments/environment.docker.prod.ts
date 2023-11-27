be0edf3aa335d6f8099c5888f6736d42f9c45023
Mario Scrocca <mario.scrocca@gmail.com>
2023-11-27 15:32:23 +0100

    Update environment.docker.prod.ts

--- coney-create/src/environments/environment.docker.prod.ts
+++ coney-create/src/environments/environment.docker.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://ns3056488.ip-213-32-26.eu/v2',
+    baseUrl: 'http://localhost:80',
     privacyUrl: 'https://www.cefriel.com/en/privacy',
-    enterprise: true
+    enterprise: false
 };
